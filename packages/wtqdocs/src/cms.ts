81ec88ba7fb5e49a2fe41aa00d62d0dc21f75146
blackPeppper <spices.digital@gmail.com>
2024-06-29 12:53:58 +0300

    ✨ feat: tinaCMS init

---
+++ packages/wtqdocs/src/cms.ts
@@ -0,0 +1,4 @@
+import blogCollections from './cms/blog'
+import docsCollections from './cms/docs'
+
+export { docsCollections, blogCollections }
---
+++ packages/wtqdocs/src/cms/blog.ts
@@ -0,0 +1,85 @@
+/**
+ * @type {import("tinacms").Collection}
+ */
+export default {
+  name: "blog",
+  label: "Blog",
+  path: "src/content/blog",
+  format: "mdx",
+  fields: [
+    {
+      type: "string",
+      name: "title",
+      label: "Title",
+      required: true,
+      ui: {
+        validate: (value) => value.length <= 60 || "It can't be more than 60 characters",
+      },
+    },
+    {
+      type: "string",
+      name: "description",
+      label: "Description",
+      required: true,
+      ui: {
+        validate: (value) => value.length <= 160 || "It can't be more than 160 characters",
+      },
+    },
+    {
+      type: "string",
+      name: "href",
+      label: "Href",
+      required: false,
+    },
+    {
+      type: "image",
+      name: "image",
+      label: "Image",
+      fields: [
+        {
+          type: "string",
+          name: "src",
+          label: "Source",
+          required: true,
+        },
+        {
+          type: "string",
+          name: "alt",
+          label: "Alt Text",
+          required: true,
+        },
+      ],
+    },
+    {
+      type: "string",
+      name: "keywords",
+      label: "Keywords",
+      list: true,
+    },
+    {
+      type: "number",
+      name: "position",
+      label: "Position",
+      required: false,
+    },
+    {
+      type: "reference",
+      name: "authors",
+      label: "Authors",
+      collections: ["authors"],
+      list: true,
+    },
+    {
+      type: "datetime",
+      name: "pubDate",
+      label: "Publish Date",
+      required: true,
+    },
+    {
+      type: "boolean",
+      name: "draft",
+      label: "Draft",
+      defaultValue: false,
+    },
+  ],
+}
