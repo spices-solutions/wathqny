c6c43bcf8823ddfd01d99bd0bff20f1f691101ac
blackPeppper <spices.digital@gmail.com>
2024-07-10 06:00:11 +0300

    hot fix

--- packages/wtqdocs/src/schema.ts
+++ packages/wtqdocs/src/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'astro/zod'
 import type { SchemaContext } from 'astro:content'
 
-export default function slugify(str: string | undefined) {
+function slugify(str: string | undefined) {
   return String(str)
     .normalize('NFKD') // split accented characters into their base characters and diacritical marks
     .replace(/[\u0300-\u036F]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
@@ -11,7 +11,6 @@ export default function slugify(str: string | undefined) {
     .replace(/\s+/g, '-') // replace spaces with hyphens
     .replace(/-+/g, '-') // remove consecutive hyphens
 }
-
 
 export const docSchema = ({ image }: SchemaContext) =>
   z
@@ -37,7 +36,28 @@ export const docSchema = ({ image }: SchemaContext) =>
       link: data.href ?? `${data.category}/${slugify(data.title)}`,
     }))
 
-export const 
+export const blogSchema = ({ image }: SchemaContext) =>
+  z
+    .object({
+      title: z.string().max(60, "it can't be more than 60 characters").min(3),
+      description: z
+        .string()
+        .max(160, "it can't be more than 160 characters")
+        .min(10),
+      href: z.string().optional(),
+      image: z
+        .object({ src: z.union([image(), z.string().url()]), alt: z.string() })
+        .optional(),
+      keywords: z.union([z.string(), z.array(z.string())]).optional(),
+      authors: z.string().optional(),
+      pubDate: z.date().transform((str: Date) => new Date(str)),
+      draft: z.boolean().default(false),
+    })
+    .strict()
+    .transform((data) => ({
+      ...data,
+      link: data.href ?? `/${slugify(data.title)}`,
+    }))
 
 export function authorSchema({ image }: SchemaContext) {
   z.object({
