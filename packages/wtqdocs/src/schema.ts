437c9f23a1666c178de0522da190aa54d9085003
blackPeppper <spices.digital@gmail.com>
2024-07-10 05:54:38 +0300

    hot fix

--- packages/wtqdocs/src/schema.ts
+++ packages/wtqdocs/src/schema.ts
@@ -1,6 +1,17 @@
 import { z } from 'astro/zod'
 import type { SchemaContext } from 'astro:content'
-import { slugify } from 'astro-toolkit/utils'
+
+export default function slugify(str: string | undefined) {
+  return String(str)
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036F]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
+}
+
 
 export const docSchema = ({ image }: SchemaContext) =>
   z
