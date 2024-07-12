d293f07ccfd8dc05c2f02543ad82eecf47f953a4
blackPeppper <spices.digital@gmail.com>
2024-07-13 01:11:26 +0300

    🧹 chore: add document entries

--- packages/wtqdocs/src/pages/search.json.js
+++ packages/wtqdocs/src/pages/search.json.js
@@ -1,23 +1,60 @@
-import { getCollection } from "astro:content";
+import { getCollection } from 'astro:content'
 
 async function getPosts() {
-  const posts = (await getCollection("blog")).sort(
-    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf()
-  );
-  // return posts;
-  return posts.map((post) => ({
-    link: post.data.link,
+  const posts = (await getCollection('blog')).sort(
+    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf(),
+  )
+  const blog = posts.map((post) => ({
     title: post.data.title,
-    description: post.data.description,
     date: post.data.pubDate,
-  }));
+    body: post.body,
+    link: post.data.link,
+  }))
+  return {
+    posts: blog,
+  }
+}
+
+async function getDocs() {
+  const docs = await getCollection('docs')
+
+  return docs.map((doc) => ({
+    title: doc.data.title,
+    // body: doc.body,
+    link: doc.data.link,
+  }))
+}
+
+async function getPostsAndDocs() {
+  const docsData = await getCollection('docs')
+  const docs = docsData.map((doc) => ({
+    title: doc.data.title,
+    // body: doc.body,
+    link: doc.data.link,
+  }))
+
+  const postsData = (await getCollection('blog')).sort(
+    (a, b) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf(),
+  )
+
+  const posts = postsData.map((post) => ({
+    title: post.data.title,
+    date: post.data.pubDate,
+    body: post.body,
+    link: post.data.link,
+  }))
+
+  return {
+    posts: posts,
+    docs: docs,
+  }
 }
 
 export async function GET() {
-  return new Response(JSON.stringify(await getPosts()), {
+  return new Response(JSON.stringify(await getPostsAndDocs()), {
     status: 200,
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  });
+  })
 }
