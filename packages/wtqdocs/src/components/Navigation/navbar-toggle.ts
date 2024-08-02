d465832e6b6794da77026676ce689a7a46179f1d
blackPeppper <spices.digital@gmail.com>
2024-08-02 15:02:44 +0300

    fix(navbar): - navbar mobile overflow - remove  unnecessary styles - refactor js logic

--- packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
+++ packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
@@ -1,10 +1,9 @@
-(()=> {
-  const navbarToggle = document.querySelector('.wtq-toggle')
-  const navbar = document.querySelector('.wtq-links')
-  
-  navbarToggle?.addEventListener('click', () => {
-    navbar?.classList.toggle('wtq-active-links')
-  })
+const navbarToggle = document.querySelector('.wtq-toggle')
+const navbar = document.querySelector('.wtq-links')
 
+navbarToggle?.addEventListener('click', () => {
+  const navbar = document.querySelector('.wtq-links')
   navbar?.classList.toggle('wtq-active-links')
-})()
+})
+
+navbar?.classList.toggle('wtq-active-links')
