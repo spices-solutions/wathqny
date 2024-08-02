3bb2c70ecf3459f9bb87fd646ea935847be2e8cb
blackPeppper <spices.digital@gmail.com>
2024-08-02 18:41:35 +0300

    perf: - remove astro config flags - remove action function - refactor script code

--- packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
+++ packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
@@ -2,8 +2,5 @@ const navbarToggle = document.querySelector('.wtq-toggle')
 const navbar = document.querySelector('.wtq-links')
 
 navbarToggle?.addEventListener('click', () => {
-  const navbar = document.querySelector('.wtq-links')
   navbar?.classList.toggle('wtq-active-links')
 })
-
-navbar?.classList.toggle('wtq-active-links')
