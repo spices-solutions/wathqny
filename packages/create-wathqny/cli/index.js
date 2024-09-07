b74f33aa0f8c547e7cf55c92dec5822fa3e231d3
abdo-spices <spices.digital@gmail.com>
2024-09-07 21:13:49 +0300

    feat: create wathqny app

--- packages/create-wathqny/cli/index.js
+++ packages/create-wathqny/cli/index.js
@@ -1,30 +1,24 @@
 #!/usr/bin/env node
 
-// Usage: npx wathqny my-app
-
 import fs  from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
+import { styleText } from 'node:util';
 
 const __dirname = path.dirname(__filename);
-// The first argument will be the project name.
+
 const projectName = process.argv[2];
 
-// Create a project directory with the project name.
 const currentDir = process.cwd();
 const projectDir = path.resolve(currentDir, projectName);
 fs.mkdirSync(projectDir, { recursive: true });
 
-// A common approach to building a starter template is to
-// create a `template` folder which will house the template
-// and the files we want to create.
 const templateDir = path.resolve(__dirname, '../template/');
 fs.cpSync(templateDir, projectDir, { recursive: true });
 
-const projectPackageJson = JSON.parse(fs.readFileSync(path.join(projectDir, 'package.json'), { encoding: "utf-8" }))
+const projectPackageJso n = JSON.parse(fs.readFileSync(path.join(projectDir, 'package.json'), { encoding: "utf-8" }))
 
-// Update the project's package.json with the new project name
 projectPackageJson.name = projectName;
 
 fs.writeFileSync(
@@ -32,5 +26,5 @@ fs.writeFileSync(
   JSON.stringify(projectPackageJson, null, 2)
 );
 
-console.log('Success! Your new project is ready.');
+console.log(styleText('green', 'Success! Your new project is ready.'));
 console.log(`Created ${projectName} at ${projectDir}`);
