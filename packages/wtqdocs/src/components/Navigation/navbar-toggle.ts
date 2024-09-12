713cf6309ebd71ba843bedb3a583c529b8e7d76d
abdo-spices <spices.digital@gmail.com>
2024-09-12 07:08:58 +0300

    ⚡: - replace sidebar icon with new one - fix arrow svg size - fix sidebar & navbar opened scroll - ui enhancement

--- packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
+++ packages/wtqdocs/src/components/Navigation/navbar-toggle.ts
@@ -2,5 +2,11 @@ const navbarToggle = document.querySelector('.wtq-toggle')
 const navbar = document.querySelector('.wtq-links')
 
 navbarToggle?.addEventListener('click', () => {
-  navbar?.classList.toggle('wtq-active-links')
+  if (navbar?.classList.contains('wtq-active-links')) {
+    navbar?.classList.remove('wtq-active-links')
+    document.body.style.overflow = ''
+  } else {
+    navbar?.classList.add('wtq-active-links')
+    document.body.style.overflow = 'hidden'
+  }
 })
