ccc05292b76fb29528e53e2177f4e3d8e09d57e8
abdo-spices <spices.digital@gmail.com>
2025-02-27 03:29:28 +0200

    feat: implement dynamic script import and update event listener in search component

--- packages/wtqdocs/src/pages/assets/injected-styles.css.ts
+++ packages/wtqdocs/src/pages/assets/injected-styles.css.ts
@@ -14,12 +14,11 @@ const Styles = WathqnyConfig.font
     font-display: swap;
     src: url("${WathqnyConfig.font}") format("woff2");
   }
-body {
-  font-family: wtqfont, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-}
 
-
+  body {
+    font-family: wtqfont, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
 `
 
   : `
@@ -32,6 +31,19 @@ body {
   align-items: normal !important;
 }
 `
+
+// @font-face {
+//   font-family: 'Adjusted Arial Fallback';
+//   src: local(Arial);
+//   size-adjust: 100%;
+//   ascent-override: 94%;
+//   descent-override: 29%;
+//   line-gap-override: 0%;
+// }
+
+// h1 {
+//   font-family: 'GeistVF', 'Adjusted Arial Fallback';
+// }
 
 export const GET: APIRoute = () => {
   return new Response(Styles, {
