84573abc0e45f6a352fa86fb1dc24652cf2da224
abdo-spices <spices.digital@gmail.com>
2025-03-04 20:22:11 +0200

    chore: update dependencies and configuration for improved performance and compatibility

--- packages/wtqdocs/src/integration.ts
+++ packages/wtqdocs/src/integration.ts
@@ -68,38 +68,38 @@ export function wathqnyPlugin(Wathqny: WathqnyConfig) {
     }),
     sitemap(),
     purgecss(),
-    AstroPWA({
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      },
-      registerType: 'autoUpdate',
-      pwaAssets: {
-        image: 'public/icon.svg',
-      },
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-        navigateFallback: "/404",
-        globPatterns: undefined
-      },
-      manifest: {
-        name: Wathqny.siteName,
-        description: Wathqny.description,
-        theme_color: '#18181B',
-        background_color: '#fff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        orientation: 'portrait',
-      },
-    }),
+    // AstroPWA({
+    //   experimental: {
+    //     directoryAndTrailingSlashHandler: true,
+    //   },
+    //   registerType: 'autoUpdate',
+    //   pwaAssets: {
+    //     image: 'public/icon.svg',
+    //   },
+    //   workbox: {
+    //     clientsClaim: true,
+    //     skipWaiting: true,
+    //     navigateFallback: "/404",
+    //     globPatterns: undefined
+    //   },
+    //   manifest: {
+    //     name: Wathqny.siteName,
+    //     description: Wathqny.description,
+    //     theme_color: '#18181B',
+    //     background_color: '#fff',
+    //     display: 'standalone',
+    //     scope: '/',
+    //     start_url: '/',
+    //     orientation: 'portrait',
+    //   },
+    // }),
     metaTags(),
-    minify({
-      logAllFiles: false,
-    }),
-    compressor({
-      gzip: true,
-      brotli: false,
-    }),
+    // minify({
+    //   logAllFiles: false,
+    // }),
+    // compressor({
+    //   gzip: true,
+    //   brotli: false,
+    // }),
   ]
 }
